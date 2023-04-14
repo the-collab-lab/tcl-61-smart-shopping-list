ca6203ff4733cad68b39f066794ca52cd45d00ad
Yufa <112290188+01001101CK@users.noreply.github.com>
2023-04-13 20:35:06 -0700

    changes after PR review

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -2,19 +2,21 @@ import { React, useState } from 'react';
 
 import { addItem } from '../api/firebase';
 
-//we need listToken (listID) passing from App.js
-
 export function AddItem({ listToken }) {
 	const [itemName, setItemName] = useState('');
 	const [daysUntilNextPurchase, setDaysUntilNextPurchase] = useState(7);
+	const [isError, setIsError] = useState(false);
+	const [isSubmitted, setIsSubmitted] = useState(false);
 
 	const submitForm = (e) => {
 		e.preventDefault();
 		try {
 			addItem(listToken, { itemName, daysUntilNextPurchase });
-			alert('Item is saved in the database.');
-		} catch (error) {
-			alert('Item is not saved in the database.');
+			setIsError(false);
+			setIsSubmitted(true);
+		} catch {
+			setIsError(true);
+			setIsSubmitted(true);
 		}
 		setItemName('');
 		setDaysUntilNextPurchase(7);
@@ -22,63 +24,61 @@ export function AddItem({ listToken }) {
 
 	const handleNameInput = (e) => setItemName(e.target.value);
 
-	const handleFrequencyInput = (e) => setDaysUntilNextPurchase(e.target.value);
+	const handleFrequencyInput = (e) => setDaysUntilNextPurchase(+e.target.value);
 
 	return (
 		<form onSubmit={(e) => submitForm(e)}>
+			<label htmlFor="itemName">Item name:</label>
+			<input
+				type="text"
+				id="itemName"
+				name="itemName"
+				value={itemName}
+				onChange={handleNameInput}
+				required
+			/>
+
 			<fieldset>
-				<label htmlFor="itemName">Item name:</label>
+				<legend htmlFor="itemFrequencyInDays">
+					How soon will you buy this again?
+				</legend>
 				<input
-					type="text"
-					id="itemName"
-					name="itemName"
-					value={itemName}
-					onChange={handleNameInput}
-					required
+					type="radio"
+					id="7"
+					name="itemFrequencyInDays"
+					value="7"
+					checked={daysUntilNextPurchase === 7}
+					onChange={handleFrequencyInput}
 				/>
-
-				<fieldset>
-					<label htmlFor="itemFrequencyInDays">
-						How soon will you buy this again?
-					</label>
-					<input
-						type="radio"
-						id="7"
-						name="itemFrequencyInDays"
-						value="7"
-						defaultChecked
-						required
-						onChange={handleFrequencyInput}
-					/>
-					<label htmlFor="7" name="soon">
-						Soon
-					</label>
-					<input
-						type="radio"
-						id="14"
-						name="itemFrequencyInDays"
-						value="14"
-						required
-						onChange={handleFrequencyInput}
-					/>
-					<label htmlFor="14" name="kindOfSoon">
-						Kind of soon
-					</label>
-					<input
-						type="radio"
-						id="30"
-						name="itemFrequencyInDays"
-						value="30"
-						required
-						onChange={handleFrequencyInput}
-					/>
-					<label htmlFor="30" name="NotSoon">
-						Not Soon
-					</label>
-				</fieldset>
-
-				<button type="submit">Add Item</button>
+				<label htmlFor="7">Soon</label>
+				<input
+					type="radio"
+					id="14"
+					name="itemFrequencyInDays"
+					value="14"
+					checked={daysUntilNextPurchase === 14}
+					onChange={handleFrequencyInput}
+				/>
+				<label htmlFor="14">Kind of soon</label>
+				<input
+					type="radio"
+					id="30"
+					name="itemFrequencyInDays"
+					value="30"
+					checked={daysUntilNextPurchase === 30}
+					onChange={handleFrequencyInput}
+				/>
+				<label htmlFor="30">Not Soon</label>
 			</fieldset>
+
+			<button type="submit">Add Item</button>
+			{isSubmitted ? (
+				isError ? (
+					<p>"Item is NOT saved in the database"</p>
+				) : (
+					<p>"Item is saved in the database"</p>
+				)
+			) : null}
 		</form>
 	);
 }
