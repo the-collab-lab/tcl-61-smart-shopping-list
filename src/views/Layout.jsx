eb6f3c73dff88c8d231494f88601446c25a494a7
Hanson Tram <htram.dev@gmail.com>
2023-05-23 16:42:51 -0700

    Add decorative lines & stars to header

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,19 +1,27 @@
 import { Outlet, NavLink } from 'react-router-dom';
+import { ReactSVG } from 'react-svg';
 
 export function Layout() {
 	return (
 		<>
 			<div className="Layout">
 				<header className="Layout-header">
-					<h1>PREDICT-A-BUY</h1>
+					<div className="title-container">
+						<ReactSVG className="star-top" src="/img/star4.svg" />
+						<ReactSVG className="star-left" src="/img/star3.svg" />
+						<h1 className="title">PREDICT-A-BUY</h1>
+						<ReactSVG className="star-right" src="/img/star1.svg" />
+					</div>
 					<nav className="Nav">
 						<NavLink to="/" className="Nav-link">
 							Home
 						</NavLink>
+						<ReactSVG className="lines-before" src="/img/lines-before.svg" />
 						<NavLink to="/list" className="Nav-link special">
 							<span className="top">Your</span>{' '}
 							<span className="bottom">List</span>
 						</NavLink>
+						<ReactSVG className="lines-after" src="/img/lines-after.svg" />
 						<NavLink to="/add-item" className="Nav-link">
 							Add Item
 						</NavLink>
