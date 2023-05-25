6bd9dda4098d616d37795cf7c44d536788f6a320
Yufa <112290188+01001101CK@users.noreply.github.com>
2023-05-24 19:05:23 -0700

    continue to group names under the same itemUrgency

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { ONE_DAY_IN_MILLISECONDS, numOfDaysBtwnDates } from '../utils';
 import { updateItem, deleteItem } from '../api/firebase';
+import deleteButton from '/img/deleteButton.png';
 
 export function ListItem({
 	name,
@@ -34,19 +35,40 @@ export function ListItem({
 	const [prevDateLastPurchased, setPrevDateLastPurchased] = useState(null);
 	const [prevDateNextPurchased, setPrevDateNextPurchased] = useState(null);
 	const [disabled, setDisabled] = useState(false);
-	// const [groupedNames, setGroupedNames] = useState({});
+	// const [groupedNames, setGroupedNames] = useState({
+	// 	inactive: [],
+	// 	soon: [],
+	// 	"kind of soon": [],
+	// 	"not soon": [],
+	// });
 
-	// useEffect(() => {
-	// 	const updatedGroupedNames = {};
-	// 	if (!updatedGroupedNames[`${itemUrgency()}`]) {
-	// 		updatedGroupedNames[`${itemUrgency()}`] = [];
-	// 	}
-	// 	updatedGroupedNames[`${itemUrgency()}`].push(name);
+	// 	useEffect(() => {
+	//     setGroupedNames(prevGroupedNames => {
+	//     const setGroupNames = { ...prevGroupedNames };
 
-	// 	setGroupedNames(updatedGroupedNames);
+	//     function urgencyList() {
+	//       const urgencies = ["inactive", "soon", "kind of soon", "not soon"];
+	//       return urgencies[Math.floor(Math.random() * urgencies.length)];
+	//     }
+
+	//     const urgency = urgencyList();
+
+	//     if (!setGroupNames[urgency]) {
+	//       setGroupNames[urgency] = [];
+	//     }
+	//     if (!setGroupNames[urgency].includes(name)) {
+	//       setGroupNames[urgency].push(...prevGroupedNames[urgency], name);
+	//     }
+
+	//     return setGroupNames; // Return the updated object as the new state
+	//   });
 	// }, [name]);
 
-	// console.log(groupedNames);
+	// console.log('groupedNames', groupedNames);
+	// console.log(`${itemUrgency()}`)
+
+	// const nameList = groupedNames[`${itemUrgency()}`].map((itemName, index) => (
+	// 			<li key={index}>{itemName}</li>));
 
 	const currentDate = new Date().getTime();
 	const dateLastPurchasedPlus24h = dateLastPurchased
@@ -86,10 +108,8 @@ export function ListItem({
 	return (
 		<>
 			<div className="item-urgency">
-				<h3>{`${itemUrgency()}`}</h3>
+				<h3 className="urgency-list">{`${itemUrgency()}`}</h3>
 			</div>
-			{/* {groupedNames[`${itemUrgency()}`].map((itemName, index) => (
-				<li className="ListItem" key={Math.random() * 100}> */}
 			<li className="ListItem">
 				<label
 					htmlFor="wasPurchased"
@@ -116,7 +136,6 @@ export function ListItem({
 					/>
 				</label>
 				<span>
-					{/* <span>{itemName}</span> */}
 					<span>{name}</span>
 				</span>
 				<button
@@ -125,53 +144,11 @@ export function ListItem({
 					}}
 					className="delete-button"
 				>
-					<svg
-						style={{ color: 'red' }}
-						width="20"
-						height="20"
-						viewBox="0 0 26 26"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							opacity="0.8"
-							d="M26 14C26 20.6274 20.6274 26 14 26C7.37258 26 2 20.6274 2 14C2 7.37258 7.37258 2 14 2C20.6274 2 26 7.37258 26 14Z"
-							fill="currentColor"
-						/>
-						<g transform="translate(3, 3)">
-							<path
-								d="M8.5 14.999C8.5 15.2752 8.27614 15.499 8 15.499C7.72386 15.499 7.5 15.2752 7.5 14.999V8.99902C7.5 8.72288 7.72386 8.49902 8 8.49902C8.27614 8.49902 8.5 8.72288 8.5 8.99902V14.999Z"
-								fill="black"
-							/>
-							<path
-								d="M10.5 14.999C10.5 15.2752 10.2761 15.499 10 15.499C9.72386 15.499 9.5 15.2752 9.5 14.999V8.99902C9.5 8.72288 9.72386 8.49902 10 8.49902C10.2761 8.49902 10.5 8.72288 10.5 8.99902V14.999Z"
-								fill="black"
-							/>
-							<path
-								d="M12.5 14.999C12.5 15.2752 12.2761 15.499 12 15.499C11.7239 15.499 11.5 15.2752 11.5 14.999V8.99902C11.5 8.72288 11.7239 8.49902 12 8.49902C12.2761 8.49902 12.5 8.72288 12.5 8.99902V14.999Z"
-								fill="black"
-							/>
-							<path
-								d="M11.5004 4.4989H11.4994H8.50072C8.50072 3.67169 9.17379 2.99951 10.0001 2.99951C10.8268 2.99951 11.4998 3.67169 11.5004 4.49792V4.4989Z"
-								fill="black"
-							/>
-							<path
-								d="M4.5 4.99902C4.22386 4.99902 4 4.77517 4 4.49902C4 4.22288 4.22386 3.99902 4.5 3.99902H15.5C15.7761 3.99902 16 4.22288 16 4.49902C16 4.77517 15.7761 4.99902 15.5 4.99902H4.5Z"
-								fill="black"
-							/>
-							<path
-								fillRule="evenodd"
-								clipRule="evenodd"
-								d="M14.5 5.5H5.5C5.22386 5.5 5 5.72386 5 6V17C5 17.2761 5.22386 17.5 5.5 17.5H14.5C14.7761 17.5 15 17.2761 15 17V6C15 5.72386 14.7761 5.5 14.5 5.5ZM6 16.5V6.5H14V16.5H6Z"
-								fill="black"
-							/>
-						</g>
-						<path
-							fillRule="evenodd"
-							clipRule="evenodd"
-							d="M13 24.5C19.3513 24.5 24.5 19.3513 24.5 13C24.5 6.64873 19.3513 1.5 13 1.5C6.64873 1.5 1.5 6.64873 1.5 13C1.5 19.3513 6.64873 24.5 13 24.5ZM13 25.5C19.9036 25.5 25.5 19.9036 25.5 13C25.5 6.09644 19.9036 0.5 13 0.5C6.09644 0.5 0.5 6.09644 0.5 13C0.5 19.9036 6.09644 25.5 13 25.5Z"
-							fill="black"
-						/>
-					</svg>
+					<img
+						src={deleteButton}
+						className="delete-button-png"
+						alt="delete-button"
+					/>
 				</button>
 			</li>
 			{/* ))} */}
